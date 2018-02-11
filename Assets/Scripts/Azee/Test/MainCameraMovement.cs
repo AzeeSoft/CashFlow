ae9f00ef3d6133ed627430838a0d9146c2537a17
Azee <aziztitu1996@gmail.com>
2018-02-10 20:02:04 -0600

    Added MiniMap. Added RetroTVEffects. GameCamera and MiniMapCamera now follow the Player.

---
+++ Assets/Scripts/Azee/Test/MainCameraMovement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraMovement : MonoBehaviour
+{
+
+    public float followSpeed = 5f;
+
+    GameObject playerGameObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        focusOnPlayer();
+    }
+
+    void focusOnPlayer()
+    {
+        transform.position = Vector3.Lerp(transform.position, new Vector3(playerGameObject.transform.position.x, playerGameObject.transform.position.y, transform.position.z), Time.deltaTime * followSpeed);
+    }
+}
---
+++ Assets/Scripts/Azee/Test/MiniMapCameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniMapCameraMovement : MonoBehaviour
+{
+
+    GameObject playerGameObject;
+
+	// Use this for initialization
+	void Start () {
+		playerGameObject = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		focusOnPlayer();
+	}
+
+    void focusOnPlayer()
+    {
+        transform.position = new Vector3(playerGameObject.transform.position.x, playerGameObject.transform.position.y, transform.position.z);
+    }
+}
