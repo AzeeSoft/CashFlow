c7c8e199ce7077d41e61ff265c817060dc2836d1
Kris J <KrisJohnson458@gmail.com>
2018-02-23 18:37:23 -0600

    explosion stuff maybee started perhaps

---
+++ Assets/Scripts/Kris/Obstacle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour {
+
+    public GameManager State;
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            DestroyObject(other);
+        }
+
+    }
+
+    public void OnExplosion()
+    {
+
+    }
+
+}
